1f30510692d9d8a8f4bbd877c2f03b9ab5d84f38
Philip Mark Deazeta <philip@tradegecko.com>
2020-05-05 11:28:09 +0800

    Add Nokogiri and httplog

--- lib/ebay_api.rb
+++ lib/ebay_api.rb
@@ -4,6 +4,7 @@ require 'oauth2'
 require 'active_support/all'
 require 'httparty'
 require 'pry'
+require 'nokogiri'
 
 require 'ebay_api/client'
 require 'ebay_api/concerns/singleton'
@@ -12,6 +13,6 @@ require 'ebay_api/resources/fulfillment/order'
 
 module EbayAPI
   class Error < StandardError; end
-  class InvalidToken < StandardError; end  
+  class InvalidToken < StandardError; end
   # Your code goes here...
 end
