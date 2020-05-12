afdf72f2aa9d7d822ea9cba5a68f7037ab975755
Philip Mark Deazeta <philip@tradegecko.com>
2020-05-12 10:18:57 +0800

    [TWEAK] Use production scopes when refreshing tokens

--- lib/ebay_api/client.rb
+++ lib/ebay_api/client.rb
@@ -119,19 +119,18 @@ module EbayAPI
     def self.fetch_new_oauth_token(oauth_client)
       api_scopes = %w[
         https://api.ebay.com/oauth/api_scope
-        https://api.ebay.com/oauth/api_scope/buy.order.readonly
+        https://api.ebay.com/oauth/api_scope/sell.marketing.readonly
+        https://api.ebay.com/oauth/api_scope/sell.marketing
+        https://api.ebay.com/oauth/api_scope/sell.inventory.readonly
         https://api.ebay.com/oauth/api_scope/sell.inventory
         https://api.ebay.com/oauth/api_scope/sell.account.readonly
+        https://api.ebay.com/oauth/api_scope/sell.account
+        https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly
         https://api.ebay.com/oauth/api_scope/sell.fulfillment
-        https://api.ebay.com/oauth/api_scope/commerce.identity.readonly
-        https://api.ebay.com/oauth/api_scope/commerce.identity.readonly
-        https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly
-        https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly
-        https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly
-        https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly
+        https://api.ebay.com/oauth/api_scope/sell.analytics.readonly
         https://api.ebay.com/oauth/api_scope/sell.finances
         https://api.ebay.com/oauth/api_scope/sell.payment.dispute
-        https://api.ebay.com/oauth/api_scope/sell.item
+        https://api.ebay.com/oauth/api_scope/commerce.identity.readonly
       ];
 
       OAuth2::AccessToken.new(
